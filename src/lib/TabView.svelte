e65f6385cce7bb7d17bfa1828624ba5b283a600e
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2023-09-09 21:14:23 +0900

    a

---
+++ src/lib/TabView.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import UiTabBar from "$lib/UITabBar.svelte";
+</script>
+
+<div class="root">
+	<div class="content">
+		<slot />
+	</div>
+	<UiTabBar />
+</div>
+
+<style>
+	.root {
+		width: 375px;
+		height: 667px;
+		display: flex;
+		flex-direction: column;
+	}
+	.content {
+		flex-grow: 2;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,10 @@
 <script lang="ts">
-	import UiTabBar from "$lib/UITabBar.svelte";
+    import TabView from "$lib/TabView.svelte";
+    import Page1 from "./Page1.svelte";
+    import Page2 from "./Page2.svelte";
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
-</svelte:head>
-
-<div class="root">
-	<div class="content">content</div>
-	<UiTabBar />
-</div>
-
-<style>
-	.root {
-		width: 375px;
-		height: 667px;
-		display: flex;
-		flex-direction: column;
-	}
-	.content {
-		flex-grow: 2;
-	}
-</style>
+<TabView>
+    <Page1 />
+    <Page2 />
+</TabView>
