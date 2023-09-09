6e65b78e0ec6cb536e0539bb9319727198412b94
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2023-09-09 23:34:31 +0900

    a

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 
+export { default as Scene } from "./Scene.svelte"
+export { default as TabView } from "./TabView.svelte"
 export { default as UITab } from "./UITab.svelte"
 export { default as UITabBar } from "./UITabBar.svelte"
+export { default as MaterialSymbol } from "./MaterialSymbol.svelte"
