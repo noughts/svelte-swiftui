d8867c764fb5bf76ba23a6c6ce40f39d89859d4e
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-11 18:51:53 +0900

    a

--- src/lib/internal/UINavigationBar.svelte
+++ src/lib/internal/UINavigationBar.svelte
@@ -1,28 +1,16 @@
 <script lang="ts">
-	import MaterialSymbol from "$lib/MaterialSymbol.svelte";
-	import type { NavigationItem, SceneContext } from "$lib/index.js";
-	import { createEventDispatcher, getContext } from "svelte";
-	const sceneContext = getContext<SceneContext>("scene");
-		const color = sceneContext.tintColor;
+	import type { NavigationItem } from "$lib/index.js";
+	import { fly } from "svelte/transition";
+	import NavigationBarItemView from "./NavigationBarItemView.svelte";
 	export let items: NavigationItem[];
-	$: topItem = items[items.length - 1];
-	$: showBackButton = items.length > 1;
-
-	const dispatch = createEventDispatcher();
 </script>
 
 <div class="root">
-	{#if showBackButton}
-		<button class="backButton" on:click={(e) => dispatch("backButtonTap")} style:color>
-			<MaterialSymbol icon="arrow_back_ios" />
-			<div class="backLabel">Back</div>
-		</button>
-	{:else}
-		<div />
-	{/if}
-	<div />
-
-	<div class="title">{topItem.title}</div>
+	{#each items as item, index}
+		<div class="item" transition:fly>
+			<NavigationBarItemView {item} showBackButton={index >= 1} on:backButtonTap />
+		</div>
+	{/each}
 </div>
 
 <style>
@@ -32,29 +20,9 @@
 		flex-shrink: 0;
 		background-color: white;
 		border-bottom: solid 0.1px rgba(0 0 0/15%);
-
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 8px;
 	}
-	.backButton {
-		all: unset;
-		display: flex;
-		align-items: center;
-		font-size: 16px;
-	}
-	.backLabel {
-		margin-left: -8px;
-	}
-	.title {
-		font-size: 16px;
-		font-weight: 600;
+	.item{
 		position: absolute;
-		inset: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		pointer-events: none;
+		inset:0;
 	}
 </style>
