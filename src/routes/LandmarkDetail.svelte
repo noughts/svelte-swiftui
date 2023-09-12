6d45ed3c24034b6977b735c35110483a20263576
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-12 13:05:23 +0900

    a

--- src/routes/LandmarkDetail.svelte
+++ src/routes/LandmarkDetail.svelte
@@ -2,18 +2,28 @@
 	import { getContext } from "svelte";
 	import type { Landmark } from "./Landmark.js";
 	import type { NavigationContext } from "$lib/index.js";
+	import MapView from "./MapView.svelte";
 
 	export let landmark: Landmark;
 
 	const navContext = getContext<NavigationContext>("navigation");
-	navContext.updateRightButtonItem({ title: "Close", bold: true, action:()=>{
-		console.log(landmark);
-	} });
+	navContext.updateRightButtonItem({
+		title: "Close",
+		bold: true,
+		action: () => {
+			console.log(landmark);
+		},
+	});
+
+	function showMap() {
+		navContext.push({ component: MapView, props: { landmark }, title: "Map" });
+	}
 </script>
 
 <div class="root">
 	<h1>Detail</h1>
 	<h2>{landmark.name}</h2>
+	<button on:click={showMap}>show map</button>
 </div>
 
 <style>
---
+++ src/routes/MapView.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Landmark } from "./Landmark.js";
+
+	export let landmark:Landmark;
+</script>
+
+<div class="root">
+	Map of {landmark.name}
+</div>
+
+<style>
+	.root{
+		height: 100%;
+		background-color: white;
+	}
+</style>
