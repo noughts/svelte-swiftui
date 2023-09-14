1c2c874bd5d4a700d0dd14bb60692b596c968a93
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2023-09-14 23:38:22 +0900

    a

--- src/routes/LandmarkDetail.svelte
+++ src/routes/LandmarkDetail.svelte
@@ -9,7 +9,7 @@
 	export function getRnd() {
 		return rnd;
 	}
-
+	viewController.navigationItem.title = landmark.name;
 	viewController.navigationItem.rightBarButtonItem = {
 		title: "Debug",
 		bold: true,
