11570eeccd8fb326e328c30b74fd2aedbc40838d
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-15 15:40:22 +0900

    a

--- src/routes/HomeNav.svelte
+++ src/routes/HomeNav.svelte
@@ -1,30 +1,30 @@
 <!-- NavigationView 継承サンプル -->
 
 <script lang="ts">
-	import NavigationView from "$lib/NavigationView.svelte";
-	import type { UINavigationController } from "$lib/UINavigationController.js";
-	import { UIView } from "$lib/UIView.js";
-	import { UIViewController } from "$lib/UIViewController.js";
-	import DemoScreen from "./DemoScreen.svelte";
+    import NavigationView from "$lib/NavigationView.svelte";
+    import type { UINavigationController } from "$lib/UINavigationController.js";
+    import { UIView } from "$lib/UIView.js";
+    import { UIViewController } from "$lib/UIViewController.js";
+    import DemoScreen from "./DemoScreen.svelte";
 
-	export let viewController: UINavigationController;
+    export let viewController: UINavigationController;
 
-	function onFabClick() {
-		viewController.present(new UIViewController(new UIView(DemoScreen), {}));
-	}
+    function onFabClick() {
+        viewController.present(new UIViewController(new UIView(DemoScreen), {}));
+    }
 </script>
 
 <NavigationView {viewController}>
-	<button class="fab" on:click={onFabClick}>FAB</button>
+    <button class="fab" on:click={onFabClick}>FAB</button>
 </NavigationView>
 
 <style>
-	.fab {
-		position: absolute;
-		width: 88px;
-		height: 88px;
-		left: 22px;
-		bottom: 22px;
-		z-index: 100;
-	}
+    .fab {
+        position: absolute;
+        width: 88px;
+        height: 88px;
+        left: 22px;
+        bottom: 22px;
+        z-index: 100;
+    }
 </style>
