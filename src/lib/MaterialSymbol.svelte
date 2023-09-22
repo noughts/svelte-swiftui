5555fc32af457264599365adcf05568c9cef1e33
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-22 15:20:18 +0900

    a

--- src/lib/MaterialSymbol.svelte
+++ src/lib/MaterialSymbol.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	export let icon: string;
+	export let weight: 100 | 200 | 300 | 400 | 500 | 600 | 700 = 400;
+	export let fill = true;
+	export let grade: -25 | 0 | 200 = 0;
+	export let opticalSize = 24;
 </script>
 
-<span class="material-symbols-rounded">{icon}</span>
+<span
+	class="material-symbols-rounded"
+	style="font-variation-settings: 'FILL' {fill ? 1 : 0}, 'wght' {weight}, 'GRAD' {grade}, 'opsz' {opticalSize}; font-size:{opticalSize}px"
+	>{icon}</span
+>
