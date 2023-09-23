69eb670ccfbcf4231382e4fafef4466ebce7a897
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2023-09-23 21:33:36 +0900

    a

---
+++ src/lib/internal/UIScrollView.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" context="module">
+	export type UIEdgeInsets = {
+		top: number;
+		bottom: number;
+	};
+</script>
+
+<script lang="ts">
+	export let contentInset: UIEdgeInsets = { top: 0, bottom: 0 };
+</script>
+
+<div class="UIScrollView">
+	<div style="padding-top: {contentInset.top}px; padding-bottom: {contentInset.bottom}px;">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.UIScrollView{
+		height: 100%;
+		overflow-y: scroll;	
+	}
+</style>
