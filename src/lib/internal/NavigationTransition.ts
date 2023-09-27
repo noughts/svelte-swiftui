b88283028e9807c1258e82fb1d02ff193065355c
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-27 18:49:42 +0900

    a

---
+++ src/lib/internal/NavigationTransition.ts
@@ -0,0 +1,26 @@
+import { cubicOut } from "svelte/easing";
+
+export function NavigationTransition(
+	node: HTMLElement,
+	{ delay = 0, duration = 400, easing = cubicOut, x = 0, y = 0, opacity = 0 } = {}
+) {
+	const style = getComputedStyle(node);
+	const target_opacity = +style.opacity;
+	const transform = style.transform === 'none' ? '' : style.transform;
+	const od = target_opacity * (1 - opacity);
+	const [xValue, xUnit] = split_css_unit(x);
+	const [yValue, yUnit] = split_css_unit(y);
+	return {
+		delay,
+		duration,
+		easing,
+		css: (t: number, u: number) => `
+			transform: ${transform} translate(${(1 - t) * xValue}${xUnit}, ${(1 - t) * yValue}${yUnit});
+			opacity: ${target_opacity - od * u}`
+	};
+}
+
+function split_css_unit(value: any) {
+	const split = typeof value === 'string' && value.match(/^\s*(-?[\d.]+)([^\s]*)\s*$/);
+	return split ? [parseFloat(split[1]), split[2] || 'px'] : [/** @type {number} */ (value), 'px'];
+}
