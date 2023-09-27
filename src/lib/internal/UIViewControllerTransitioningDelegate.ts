a73a3d23c5c03dd9e15c793e05b206e13a13a687
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-09-27 17:50:21 +0900

    a

--- src/lib/internal/UIViewControllerTransitioningDelegate.ts
+++ src/lib/internal/UIViewControllerTransitioningDelegate.ts
@@ -1,14 +1,28 @@
 import type { UIViewController } from "$lib/index.js";
+import type { UIPercentDrivenInteractiveTransition } from "./UIPercentDrivenInteractiveTransition.js";
 import type { UIPresentationController } from "./UIPresentationController.js";
 import type { UIViewControllerAnimatedTransitioning } from "./UIViewControllerAnimatedTransitioning.js";
 
 export interface UIViewCOntrollerTransitioningDelegate {
+
+	// --- トランジション・アニメーター・オブジェクトの取得
+
 	// ViewControllerを表示する際に使用する遷移アニメータオブジェクトをデリゲートに問い合わせます。
 	animationControllerForPresented?: (presented: UIViewController, presenting: UIViewController, source: UIViewController) => UIViewControllerAnimatedTransitioning;
 
-	// ビューコントローラを終了する際に使用する遷移アニメータオブジェクトをデリゲートに問い合わせます。
-	animationControllerForDismissed?:(dismissed:UIViewController) => UIViewControllerAnimatedTransitioning;
+	// ViewControllerを終了する際に使用する遷移アニメータオブジェクトをデリゲートに問い合わせます。
+	animationControllerForDismissed?: (dismissed: UIViewController) => UIViewControllerAnimatedTransitioning;
 
-	// ビューコントローラを表示する際のビュー階層の管理に使用するカスタムプレゼンテーションコントローラをデリゲートに問い合わせます。
-	presentationController?:(presented:UIViewController, presenting:UIViewController, source:UIViewController) => UIPresentationController;
+	// --- インタラクティブ・アニメーター・オブジェクトの取得
+
+	// ViewControllerを表示する際に使用するインタラクティブアニメータオブジェクトをデリゲートに問い合わせます。
+	interactionControllerForPresentation?: (animator: UIViewControllerAnimatedTransitioning) => UIPercentDrivenInteractiveTransition;
+
+	// ViewControllerを終了する際に使用するインタラクティブアニメータオブジェクトをデリゲートに問い合わせます。
+	interactionControllerForDismissal?:(animator:UIViewControllerAnimatedTransitioning) => UIPercentDrivenInteractiveTransition;
+
+	// --- Getting the custom presentation controller
+
+	// ViewControllerを表示する際のビュー階層の管理に使用するカスタムプレゼンテーションコントローラをデリゲートに問い合わせます。
+	presentationController?: (presented: UIViewController, presenting: UIViewController, source: UIViewController) => UIPresentationController;
 }
