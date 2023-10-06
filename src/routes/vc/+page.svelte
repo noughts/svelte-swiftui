87023237aaf409c5d2ce891a52b00b02f37646da
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-10-06 18:02:54 +0900

    a

---
+++ src/routes/vc/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { UINavigationController } from "$lib/UINavigationController.js";
+	import { UIView } from "$lib/UIView.js";
+	import { UIViewController } from "$lib/UIViewController.js";
+	import { App, ViewControllerRenderer } from "$lib/index.js";
+	import "../../globals.css";
+	import LandmarkList from "../LandmarkList.svelte";
+</script>
+
+<div class="root">
+	<ViewControllerRenderer viewController={new UIViewController(new UIView(LandmarkList))} />
+</div>
+
+
+<style>
+	.root{
+		height:100dvh;
+	}
+</style>
