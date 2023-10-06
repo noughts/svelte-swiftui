e8161406b6484aa3cab9be59abfcde8aafc5d309
Koichi Yamamoto <noughts@users.noreply.github.com>
2023-10-06 18:22:58 +0900

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,9 @@
     import HomeNav from "./HomeNav.svelte";
     import LandmarkList from "./LandmarkList.svelte";
     import ModalTestScreen from "./ModalTestScreen.svelte";
+    import ShortContentScreen from "./ShortContentScreen.svelte";
+    import TestActionScreen from "./TestActionScreen.svelte";
+    import TikTokScreen from "./TikTokScreen.svelte";
 </script>
 
 <App
@@ -21,24 +24,21 @@
                 tabBarItem: { title: "wo/NavBar", icon: { name: "home" } },
             }
         ),
-        // new UINavigationController(new UIViewController(new UIView(LandmarkList)), null, {
-        //     tabBarItem: {
-        //         title: "よく使う項目",
-        //         icon: { name: "partly_cloudy_day", fill: false },
-        //     },
-        // }),
+        new UINavigationController(new UIViewController(new UIView(LandmarkList)), null, {
+            tabBarItem: {
+                title: "よく使う項目",
+                icon: { name: "partly_cloudy_day", fill: false },
+            },
+        }),
         new UIViewController(new UIView(ModalTestScreen), {
             tabBarItem: { title: "Modal", icon: { name: "star", weight: 400 } },
         }),
-        // new UIViewController(new UIView(TestActionScreen), {
-        //     tabBarItem: { title: "Actions", icon: { name: "star", weight: 400 } },
-        // }),
 
-        // new UINavigationController(new UIViewController(new UIView(ShortContentScreen)), null, {
-        //     tabBarItem: { title: "履歴", icon: { name: "search", fill: true } },
-        // }),
-        // new UIViewController(new UIView(TikTokScreen), {
-        //     tabBarItem: { title: "TikTok", icon: { name: "star", fill: true } },
-        // }),
+        new UINavigationController(new UIViewController(new UIView(ShortContentScreen)), null, {
+            tabBarItem: { title: "履歴", icon: { name: "search", fill: true } },
+        }),
+        new UIViewController(new UIView(TikTokScreen), {
+            tabBarItem: { title: "TikTok", icon: { name: "star", fill: true } },
+        }),
     ])}
 />
--- src/routes/TikTokScreen.svelte
+++ src/routes/TikTokScreen.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	import { UIScrollView, UIViewController } from "$lib/index.js";
+	import { ScrollView, type UIViewController } from "$lib/index.js";
 	import TikTokCell from "./TikTokCell.svelte";
-	export let viewController:UIViewController;
+	export let viewController: UIViewController;
 	const colors: string[] = ["red", "blue", "green", "yellow", "purple"];
 </script>
 
 <div class="root">
-	<UIScrollView isPagingEnabled={true} showsScrollIndicator={false} contentInset={{ top: 0, bottom: 49 }}>
+	<ScrollView
+		isPagingEnabled={true}
+		showsScrollIndicator={false}
+		contentInset={{ top: 0, bottom: 49 }}
+	>
 		<div class="cells">
 			{#each colors as color}
 				<TikTokCell {color} />
 			{/each}
 		</div>
-	</UIScrollView>
+	</ScrollView>
 </div>
 
 <style>
